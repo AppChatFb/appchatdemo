d4bde31e7d1faad5be102e1ce8e53e71880d681e
Phung Tuan Anh (VTI.D5.Intern) <anh.phungtuan@vti.com.vn>
2020-02-24 17:41:21 +0700

    tuan anh sua login,oncreate,forget pass

--- AppChatFB/app/src/main/java/com/example/appchatfb/DangNhap.java
+++ AppChatFB/app/src/main/java/com/example/appchatfb/DangNhap.java
@@ -1,35 +1,50 @@
 package com.example.appchatfb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.appchatfb.databinding.ActivityDangNhapBinding;
 import com.example.appchatfb.view.MainActivity;
+import com.example.appchatfb.viewmodel.ActivityDangNhapViewModel;
 
-public class DangNhap extends AppCompatActivity {
-    Button btnSingIn,btnRegis;
+public class DangNhap extends AppCompatActivity implements ClickDangNhap {
+    private ActivityDangNhapViewModel viewModel;
+    private ActivityDangNhapBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dang_nhap);
-        btnSingIn=findViewById(R.id.btn_sing_in);
-        btnRegis=findViewById(R.id.btn_regis);
-        btnSingIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(DangNhap.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-        btnRegis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(DangNhap.this,Register.class);
-                startActivity(intent);
-            }
-        });
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_dang_nhap);
+        viewModel=new ViewModelProvider(this).get(ActivityDangNhapViewModel.class);
+        binding.setItemclick(this);
+    }
+
+    @Override
+    public void clickDangNhap() {
+        if(viewModel.checkLogIn(binding.etEmail.getText().toString().trim(),binding.etPass.getText().toString().trim()))
+        {
+            Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(DangNhap.this, MainActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void chuyenmanhinh() {
+        Intent intent=new Intent(DangNhap.this,Register.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void chuyenMHForgetPass() {
+        Intent intent=new Intent(DangNhap.this,ActivityForgetPassWord.class);
+        startActivity(intent);
     }
 }
--- AppChatFB/app/src/main/java/com/example/appchatfb/Register.java
+++ AppChatFB/app/src/main/java/com/example/appchatfb/Register.java
@@ -6,31 +6,29 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.appchatfb.databinding.ActivityRegisterBinding;
 import com.example.appchatfb.interfacefunc.Event;
 import com.example.appchatfb.viewmodel.LoginViewModel;
 
-public class Register extends AppCompatActivity{
+public class Register extends AppCompatActivity implements Event {
     private ActivityRegisterBinding binding;
     private LoginViewModel viewModel;
-    Event event;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_register);
         viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
-        event=new Event() {
-            @Override
-            public void onClick() {
-                if(viewModel.register(binding.regisUser.getText().toString(),binding.etEmail.getText().toString(),binding.etPass.getText().toString())){
-                    Intent intent=new Intent(Register.this,DangNhap.class);
-                    startActivity(intent);
-                }
-            }
-        };
-        binding.setEvent(event);
+        binding.setEvent(this);
     }
 
-
+    @Override
+    public void onClick() {
+        if(viewModel.register(binding.regisUser.getText().toString().trim(),binding.etEmail.getText().toString().trim(),binding.etPass.getText().toString().trim())){
+            Toast.makeText(this, "Đăng kí thành công", Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(Register.this,DangNhap.class);
+            startActivity(intent);
+        }
+    }
 }

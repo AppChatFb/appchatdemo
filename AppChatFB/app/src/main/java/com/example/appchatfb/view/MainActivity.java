e1aaf29849d2c0ed1ab2993c4a2331d6319db328
Nguyen Dan <dan.nguyenvan@vti.com.vn>
2020-02-25 10:22:37 +0700

    update code

--- AppChatFB/app/src/main/java/com/example/appchatfb/view/MainActivity.java
+++ AppChatFB/app/src/main/java/com/example/appchatfb/view/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.appchatfb.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.appchatfb.Adapter.ViewPagerAdapter;
 import com.example.appchatfb.R;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
-    ViewPager vpChat;
-    TabLayout tlChat;
-    ViewPagerAdapter adapter;
-    Toolbar toolbar;
+public class MainActivity extends AppCompatActivity{
+   private ViewPager vpChat;
+   private TabLayout tlChat;
+   private ViewPagerAdapter adapter;
+   private Toolbar toolbar;
+    private FragmentManager fragmentManager;
+    private final int REQUEST_CODE_CAMERA = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         vpChat.setAdapter(adapter);
         tlChat=findViewById(R.id.tb_Chat);
         tlChat.setupWithViewPager(vpChat);
+        fragmentManager = getSupportFragmentManager();
     }
 
     @Override
@@ -35,4 +42,25 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menusetting,menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.acc_setting:
+                AccSettingFragment fragment = new AccSettingFragment();
+                fragmentManager.beginTransaction().setCustomAnimations(R.anim.slide_in_up,R.anim.slide_in_down,R.anim.slide_out_down,R.anim.slide_out_up)
+                        .replace(R.id.container,fragment).addToBackStack(null).commit();
+                break;
+            case R.id.all_user:
+                break;
+            case R.id.log_out:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
 }
